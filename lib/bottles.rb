2eb92c16c639d81d90ef1abb68c950dac3f927aa
Kyle Sponheim <kyle.sponheim@gmail.com>
2017-12-16 14:27:29 -0800

    interpolate number

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -16,15 +16,15 @@ class Bottles
       "Go to the store and buy some more, " +
       "99 bottles of beer on the wall.\n"
     when 1
-      "1 bottle of beer on the wall, " +
-      "1 bottle of beer.\n" +
+      "#{number} bottle of beer on the wall, " +
+      "#{number} bottle of beer.\n" +
       "Take it down and pass it around, " +
       "no more bottles of beer on the wall.\n"
     when 2
-      "2 bottles of beer on the wall, " +
-      "2 bottles of beer.\n" +
+      "#{number} bottles of beer on the wall, " +
+      "#{number} bottles of beer.\n" +
       "Take one down and pass it around, " +
-      "1 bottle of beer on the wall.\n"
+      "#{number - 1} bottle of beer on the wall.\n"
     else
       "#{number} bottles of beer on the wall, " +
       "#{number} bottles of beer.\n" +
