149e19c069ceccd1555c03905db522d13dbe2b2e
Kyle Sponheim <kyle.sponheim@gmail.com>
2017-12-16 15:08:34 -0800

    add concept of action

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -13,13 +13,22 @@ class Bottles
     when 0
       "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
       "#{quantity(number)} #{container(number)} of beer.\n" +
-      "Go to the store and buy some more, " +
+      "#{action(number)}" +
       "99 #{container(number)} of beer on the wall.\n"
     else
       "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
       "#{quantity(number)} #{container(number)} of beer.\n" +
-      "Take #{pronoun(number)} down and pass it around, " +
+      "#{action(number)}" +
       "#{quantity(number - 1)} #{container(number - 1)} of beer on the wall.\n"
+    end
+  end
+
+  def action(number)
+    case number
+    when 0
+      "Go to the store and buy some more, "
+    else
+      "Take #{pronoun(number)} down and pass it around, "
     end
   end
 
