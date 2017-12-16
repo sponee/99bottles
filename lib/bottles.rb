53bdc4c8996d2e5c991c642bd910d29d68952b47
Kyle Sponheim <kyle.sponheim@gmail.com>
2017-12-16 14:34:21 -0800

    replace all hardcoded bottle(s) with container

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -11,18 +11,18 @@ class Bottles
   def verse(number)
     case number
     when 0
-      "No more bottles of beer on the wall, " +
-      "no more bottles of beer.\n" +
+      "No more #{container(number)} of beer on the wall, " +
+      "no more #{container(number)} of beer.\n" +
       "Go to the store and buy some more, " +
-      "99 bottles of beer on the wall.\n"
+      "99 #{container(number)} of beer on the wall.\n"
     when 1
-      "#{number} bottle of beer on the wall, " +
-      "#{number} bottle of beer.\n" +
+      "#{number} #{container(number)} of beer on the wall, " +
+      "#{number} #{container(number)} of beer.\n" +
       "Take it down and pass it around, " +
-      "no more bottles of beer on the wall.\n"
+      "no more #{container(number - 1)} of beer on the wall.\n"
     else
-      "#{number} bottles of beer on the wall, " +
-      "#{number} bottles of beer.\n" +
+      "#{number} #{container(number)} of beer on the wall, " +
+      "#{number} #{container(number)} of beer.\n" +
       "Take one down and pass it around, " +
       "#{number-1} #{container(number - 1)} of beer on the wall.\n"
     end
