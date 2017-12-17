f353372379cbe72da2583c0a85f8de1f714e85a4
Kyle Sponheim <kyle.sponheim@gmail.com>
2017-12-16 16:26:42 -0800

    copy Bottle methods to BottleNumber

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -57,4 +57,43 @@ class Bottles
 end
 
 class BottleNumber
+  def successor(number)
+    if number == 0
+      99
+    else
+      number - 1
+    end
+  end
+
+  def action(number)
+    if number == 0
+      "Go to the store and buy some more, "
+    else
+      "Take #{pronoun(number)} down and pass it around, "
+    end
+  end
+
+  def quantity(number)
+    if number == 0
+      "no more"
+    else
+      number.to_s
+    end
+  end
+
+  def container(number)
+    if number == 1
+      "bottle"
+    else
+      "bottles"
+    end
+  end
+
+  def pronoun(number)
+    if number == 1
+      "it"
+    else
+      "one"
+    end
+  end
 end
