db2664974fde1c056d9a291856972a5d46cc6aac
Kyle Sponheim <kyle.sponheim@gmail.com>
2017-12-17 15:26:19 -0800

    create BottleNumber

--- lib/original_bottles.rb
+++ lib/original_bottles.rb
@@ -17,19 +17,43 @@ class OriginalBottles
   private
 
   def pluralize_bottle(bottles_count)
-    if bottles_count == 1
-      return "#{(bottles_count)} bottle"
-    elsif bottles_count == 0
+    BottleNumber.new(bottles_count).pluralize_bottle
+  end
+
+  def singularize_take(bottles_count)
+    BottleNumber.new(bottles_count).singularize_take(bottles_count)
+  end
+
+  def print_first_verse_line(bottles_count)
+    BottleNumber.new(bottles_count).print_first_verse_line(bottles_count)
+  end
+
+  def print_second_verse_line(bottles_count)
+    BottleNumber.new(bottles_count).print_second_verse_line(bottles_count)
+  end
+end
+
+class BottleNumber
+  attr_reader :bottle_number
+
+  def initialize(bottle_number)
+    @bottle_number = bottle_number
+  end
+
+  def pluralize_bottle
+    if @bottle_number == 1
+      return "#{(@bottle_number)} bottle"
+    elsif @bottle_number == 0
       return "no more bottles"
-    elsif bottles_count == 6
+    elsif @bottle_number == 6
       return "1 six-pack"
     else
-      return "#{(bottles_count)} bottles"
+      return "#{(@bottle_number)} bottles"
     end
   end
 
   def singularize_take(bottles_count)
-    if bottles_count == 0
+    if @bottle_number == 0
       return "Take it"
     else
       return "Take one"
@@ -37,18 +61,18 @@ class OriginalBottles
   end
 
   def print_first_verse_line(bottles_count)
-    if bottles_count > 0
-      return "#{pluralize_bottle(bottles_count)} of beer on the wall, #{pluralize_bottle(bottles_count)} of beer.\n"
+    if @bottle_number > 0
+      return "#{pluralize_bottle} of beer on the wall, #{pluralize_bottle} of beer.\n"
     else
       return "No more bottles of beer on the wall, no more bottles of beer.\n"
     end
   end
 
   def print_second_verse_line(bottles_count)
-    if bottles_count == -1
+    if @bottle_number == -1
       return "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
     else
-      return "#{singularize_take(bottles_count)} down and pass it around, #{pluralize_bottle(bottles_count)} of beer on the wall.\n"
+      return "#{singularize_take(@bottle_number)} down and pass it around, #{pluralize_bottle} of beer on the wall.\n"
     end
   end
 end
