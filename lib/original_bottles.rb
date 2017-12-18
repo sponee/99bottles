6d203b19b836b36898cca7f469f407f9355aa3da
Kyle Sponheim <kyle.sponheim@gmail.com>
2017-12-17 16:49:09 -0800

    use a single print_verse_method

--- lib/original_bottles.rb
+++ lib/original_bottles.rb
@@ -2,8 +2,9 @@ require 'pry'
 class OriginalBottles
 
   def verse(starting_bottles_count)
-    verse = print_first_verse_line(starting_bottles_count) + print_second_verse_line(starting_bottles_count - 1)
-    return verse
+    bottle_number = BottleNumber.new(starting_bottles_count)
+    second_bottle_number = BottleNumber.new(starting_bottles_count - 1)
+    return bottle_number.print_verse_line
   end
 
   def verses(starting_bottles_count, ending_bottles_count)
@@ -41,14 +42,26 @@ class BottleNumber
   end
 
   def pluralize_bottle
-    if @bottle_number == 1
+    if bottle_number == 0
+      "no more bottles"
+    elsif bottle_number == 1
       return "#{(@bottle_number)} bottle"
-    elsif @bottle_number == 0
-      return "no more bottles"
-    elsif @bottle_number == 6
+    elsif bottle_number == 6
       return "1 six-pack"
     else
       return "#{(@bottle_number)} bottles"
+    end
+  end
+
+  def pluralize_decremented_bottle
+    if bottle_number - 1 == 0
+      "no more bottles"
+    elsif bottle_number - 1 == 1
+      return "#{(@bottle_number - 1)} bottle"
+    elsif bottle_number - 1 == 6
+      return "1 six-pack"
+    else
+      return "#{(@bottle_number - 1)} bottles"
     end
   end
 
@@ -60,19 +73,21 @@ class BottleNumber
     end
   end
 
-  def print_first_verse_line
-    if @bottle_number == 0
-      return "No more bottles of beer on the wall, no more bottles of beer.\n"
+  def singularize_decremented_take
+    if @bottle_number - 1 == 0
+      return "Take it"
     else
-      return "#{pluralize_bottle} of beer on the wall, #{pluralize_bottle} of beer.\n"
+      return "Take one"
     end
   end
 
-  def print_second_verse_line
-    if @bottle_number == -1
-      return "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+  def print_verse_line
+    if @bottle_number == 0
+      "No more bottles of beer on the wall, no more bottles of beer.\n" + 
+      "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
     else
-      return "#{singularize_take} down and pass it around, #{pluralize_bottle} of beer on the wall.\n"
+      "#{pluralize_bottle} of beer on the wall, #{pluralize_bottle} of beer.\n" + 
+      "#{singularize_decremented_take} down and pass it around, #{pluralize_decremented_bottle} of beer on the wall.\n"
     end
   end
 end
